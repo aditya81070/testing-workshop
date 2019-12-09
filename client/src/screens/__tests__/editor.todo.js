eee26ac1758196645ebd6eb16b93c5d6edae29f1
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-09 11:49:26 +0530

    add unit test for editor screen

--- client/src/screens/__tests__/editor.todo.js
+++ client/src/screens/__tests__/editor.todo.js
@@ -1,18 +1,46 @@
-test('calls onSubmit with the username and password when submitted', () => {
-  // Arrange
-  // create a fake user, post, history, and api
-  //
-  // use ReactDOM.render() to render the editor to a div
-  //
-  // fill out form elements with your fake post
-  //
-  // Act
-  // submit form
-  //
-  // wait for promise to settle
-  //
-  // Assert
-  // ensure the create function was called with the right data
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Editor from '../editor.todo'
+import * as utilsMock from '../../utils/api'
+
+jest.mock('../../utils/api', () => {
+  return {
+    posts: {
+      create: jest.fn(() => Promise.resolve()),
+    },
+  }
+})
+
+const flushPromises = () => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 0)
+  })
+}
+test('calls onSubmit with the username and password when submitted', async () => {
+  const container = document.createElement('div')
+  const fakeUser = {id: 'foobar'}
+  const fakeHistory = {
+    push: jest.fn(),
+  }
+  ReactDOM.render(<Editor user={fakeUser} history={fakeHistory} />, container)
+  const form = container.querySelector('form')
+  const {title, content, tags} = form.elements
+  title.value = 'react is awesome'
+  content.value = 'Like a lot ...sorta'
+  tags.value = 'awesome,    lib , javascript'
+  const submit = new Event('submit')
+  form.dispatchEvent(submit)
+  await flushPromises()
+  expect(fakeHistory.push).toHaveBeenCalledTimes(1)
+  expect(fakeHistory.push).toHaveBeenCalledWith('/')
+  expect(utilsMock.posts.create).toHaveBeenCalledTimes(1)
+  expect(utilsMock.posts.create).toHaveBeenCalledWith({
+    authorId: fakeUser.id,
+    title: title.value,
+    content: content.value,
+    tags: ['awesome', 'lib', 'javascript'],
+    date: expect.any(String),
+  })
 })
 
 // TODO later...
